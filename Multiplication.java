14c2439182e3eb9c3734f314644a7f2997ed098f
vinay741 <vinaypollakayala@gmail.com>
2024-12-17 14:22:34 +0530

    multiplication of numbers

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+public class Multiplication {
+    public static void main(String[] args)
+ {
+    int num1 = 5;
+     int num2 = 10;
+      int result = multiply(num1, num2);
+       System.out.println("Multiplication of " + num1 + " and " + num2 + " is: " + result);
+    }
+ public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
